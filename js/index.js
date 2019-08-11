dc3fa82b438739203525021b087bab2f031952e3
Warren Rawlings <rawlings.warren@gmail.com>
2019-08-10 22:46:05 -0400

    the nav bar at top is good

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+//add a break HTML element to the page 
+//const break = document.createElement('br')
+//document.querySelector('services-header')appendChild(break)
+const navLinks = document.querySelectorAll(`.nav-link`)
+
+function backgroundGrn() {
+    event.target.style.backgroundColor = `green`
+    console.log(`the thing happened!`)
+}
+navLinks[0].addEventListener(`click`, backgroundGrn)
+navLinks[1].addEventListener(`dblclick`, backgroundGrn)
+navLinks[2].addEventListener(`mouseover`, backgroundGrn)
+navLinks[3].addEventListener(`keydown`, backgroundGrn)
+    //I had to click the target to make sure it was in focus, then press any key.
